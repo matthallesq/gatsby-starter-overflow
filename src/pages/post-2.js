4da8ec2f93832f5cb42739d53afad8e8ff035695
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-08-21 12:23:08 +1000

    Create post-2.js

---
+++ src/pages/post-2.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import Footer from '../components/Footer';
+import { Link } from 'gatsby';
+
+
+const IndexPage = () => (
+  <Layout>
+    <article className="container box style3">
+      <header>
+        <h2>Where is the citizen in transformation?</h2>
+        <h3>Putting the last mile first in local government </h3>
+        </header>
+        <section>
+
+        <p>
+        The work we’ve have been doing at Briarbird of late often involves engagements with local governments, many of which provide insight into their respective digital transformation programs.  </p>
+ 
+  <p>One of the surprising observations we’ve made is not all of these programs feature or prioritise the delivery of direct service benefits to citizens through digital channels, rather they tend to focus on strategic objectives like obtaining a single view of the customer for some future yet to be determined benefit.</p>
+  
+  <p>Ironically some of the councils with the smaller coffers seem to have the best is class digital experiences, they appear to have put the last mile first, in that their focus has been on improving the customer experience for citizens through their digital channels.</p>
+  
+  <p> I understand the foundation work and heavy lifting required to deliver a digital transformation program, there has to be substantial investments made into technology, processes and people to uplift the capabilities of council, converting paper-driven or legacy IT processes to digital workflows, adapting to new ways of working etc. However, in some cases, it would appear little of this is being done for the direct benefit of servicing citizens.  </p>
+ 
+  <p>That it not to say there aren’t clear benefits, in fact, councils have been making some of the most substantive improvements ever e.g. I recently spoke with a planning department that has been through a digital transformation and halved steps in the process resulting in reducing the time it takes to obtain a planning approval by 50 days!   </p>
+  
+  <p>However, if you're digital transformation program isn’t prioritising delivering services that improve the way citizens interact with council (aka the last mile), how will you know if your digital program is or will be a success? </p>
+  
+  <p> The answer lies in blueprinting the digital transformation to include:  </p>
+
+  <ol>
+  
+<li>	Conducting any forms of research you can to gather insight into the problems citizens face and take these into the design process </li>
+<li>	Uplifting human-centred design capability to understand and design customer journeys for both simple and complex business processes</li>
+<li>	Ensuring your platforms and vendors demonstrate value by prioritising the end to end delivery of services to citizens, starting at the procurement phase. </li>
+
+ </ol>
+  
+ <p> I’m curious to know your thoughts about putting the last mile first in local government?  </p>          
+
+      
+      </section>
+  
+    </article>
+
+    <Footer />
+  </Layout>
+);
+
+export default IndexPage;
