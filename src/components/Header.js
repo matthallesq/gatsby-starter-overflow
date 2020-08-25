a2c73e06509b1a2a50e1c23a02ce8406e4bdd98e
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-08-25 12:27:21 +1000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ export default function Header({ title, heading, avatar }) {
       <header>
         <h1>{config.authorName}</h1>
         <p>{config.heading}</p>
+        <p>I relish the opportunity to help businesses adopting contemporary approaches to customer and product development, delivering insight, improvement and value. I draw on pragmatic experience in strategy, design and delivery within digital transformation programs as well as creating new products for consumers and business.</p>
       </header>
       <footer>
         <Scroll type="id" element="banner">
