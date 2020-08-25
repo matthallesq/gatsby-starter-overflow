e290ce26cf2120ec1d612593ac7ff1b8e3541810
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-08-26 09:29:43 +1000

    Update post-4.js

--- src/pages/post-4.js
+++ src/pages/post-4.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import Layout from '../components/Layout';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Link } from 'gatsby';
 
