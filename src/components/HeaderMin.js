025033ca43435b684c7dc901218079147816e856
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-08-26 10:04:00 +1000

    esse

--- src/components/HeaderMin.js
+++ src/components/HeaderMin.js
@@ -4,7 +4,7 @@ import config from '../../config';
 
 export default function HeaderMin({ title, heading, avatar }) {
   return (
-    <section>
+    <section id="header-min">
       <header>
         <h1>{config.authorName}</h1>
       </header>
