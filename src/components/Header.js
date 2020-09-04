43be62b762387ab1d55a8157f74e6818ef637784
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-09-04 13:23:07 +1000

    esse

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Scroll from './Scroll';
 import config from '../../config';
+import logo from '../assets/images/logo.png';
+
 
 export default function Header({ title, heading, avatar }) {
   return (
     <section id="header">
       <header>
-        <h1>{config.authorName}</h1>
+      <img src={logo} alt="" />
         <p>{config.heading}</p>
       </header>
       <footer>
